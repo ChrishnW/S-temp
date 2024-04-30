063c52c09302ee8d2085a4074588179555e7f5c1
ChrishnSK <c.lopez@glorylocal.com.ph>
2024-04-30 09:45:45 +0800

    updated

--- include/auth.php
+++ include/auth.php
@@ -1,5 +1,4 @@
 <?php
-	include("connect.php");
 	// Start session
 	session_start();
 
--- include/connect.php
+++ include/connect.php
@@ -4,7 +4,5 @@
   $db_database	= 'gtms'; //db name
   $db_pass		= 'p@55w0rd$$$'; //db password
 
-  $con = mysqli_connect($db_host,$db_user,$db_pass,$db_database) or die('Unable to establish a DB connection');
-  date_default_timezone_set('Asia/Manila');
-  $systemtime = date('Y-m-d H:i:s');
+  $con = mysqli_connect($db_host,$db_user,$db_pass,$db_database) or die('<script>alert("Error 400 Bad Request!"); history.back();</script>');
 ?>
